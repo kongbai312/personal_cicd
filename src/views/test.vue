9f36ea1f729627315c4d0812399a18a36fa5d828
Blank_空白 <312781556@qq.com>
2023-10-18 19:20:36 +0800

    添加彩蛋

--- src/views/test.vue
+++ src/views/test.vue
@@ -1,30 +1,58 @@
 <template>
-    <div>
-      Home
-      <el-button>Default</el-button>
-      <el-button type="primary" size="large">Primary</el-button>
-      <el-button type="success">Success</el-button>
-      <el-button type="info">Info</el-button>
-      <el-button type="warning">Warning</el-button>
-      <el-button type="danger">Danger</el-button>
-      <pr_button>123</pr_button>
-    </div>
-    <div class="test"></div>
-    <span class="text">测试</span>
-  </template>
+  <div class="a-text">1</div>
+  <div class="a-img1">2</div>
+  <div class="a-text">3</div>
+  <div class="a-img2">4</div>
+  <div class="a-text">5</div>
+  <div class="a-img3">6</div>
+  <div class="a-text">7</div>
+</template>
+
+<script setup lang="ts">
+import imgBase64 from '@/assets/json/imgBase64.json';
+
+  //设置打印的输入样式
+  const setConsole = () => {
+    console.log('%c %c竟然被你发现此地，那么只好...',`background:url(${imgBase64.love}) no-repeat;padding:30px;background-size:100%;background-position:center;`,'color:red;font-size:20px;height:60px;line-height:60px;')
+    console.log('%c2023年%c10月18日','background: #35495e; padding: 4px; border-radius: 3px 0 0 3px; color: #fff','background: #41b883; padding: 4px; border-radius: 0 3px 3px 0; color: #fff')
+  }
+
+  onMounted(() => {
+    setConsole()
+  })
+</script>
   
-  <script setup lang='ts'>
-     
-  </script>
-  
-  <style lang="scss" scoped>
-    .test{
-      background-color: red;
-      width: 90px;
-      height: 90px;
-    }
-    .text{
-      font-size: 16px;
-    }
-  </style>
+<style lang="scss" scoped>
+@media (min-width: 600px) {
+  div {
+    height: 100vh;
+    background: rgba(0, 0, 0, .7);
+    color: #fff;
+    line-height: 100vh;
+    text-align: center;
+    font-size: 20vh;
+  }
+
+  .a-img1 {
+    background-image: url(https://pic.imgdb.cn/item/64cdf7521ddac507cc0fa9ef.jpg);
+    background-attachment: fixed;
+    background-size: cover;
+    background-position: center center;
+  }
+
+  .a-img2 {
+    background-image: url(https://pic.imgdb.cn/item/64ae6b561ddac507cc1464c1.jpg);
+    background-attachment: fixed;
+    background-size: cover;
+    background-position: center center;
+  }
+
+  .a-img3 {
+    background-image: url(https://pic.imgdb.cn/item/64cdf3d51ddac507cc05b6dc.jpg);
+    background-attachment: fixed;
+    background-size: cover;
+    background-position: center center;
+  }
+}
+</style>
   
