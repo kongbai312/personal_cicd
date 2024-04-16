7241af59de7e203c4c8752cd4235c0e73aeae158
kongbai312 <86761381+kongbai312@users.noreply.github.com>
2024-04-16 15:38:10 +0000

    Deploying to release from @ kongbai312/personal_ts@69831cc95096368e0f26e6c4c6eb39d319025b3b 🚀

--- assets/getIp-ac0bd5a6.js
+++
@@ -1,1 +0,0 @@
-import{r}from"./request-5586d264.js";const s=async()=>{let t={},a=await r({url:"https://api.ipify.org/?format=json"});t.ip=a.ip;let p=await r({url:`https://api.vore.top/api/IPdata?ip=${t.ip}`});return t.address=p.adcode.c,t};export{s as g};
--- assets/index-12ebf044.js
+++
@@ -1,1 +0,0 @@
-import{S as r,f as t}from"./index-b8e0e903.js";const n=({from:a,replacement:o,scope:s,version:m,ref:p,type:c="API"},e)=>{r(()=>t(e),f=>{},{immediate:!0})};export{n as u};
